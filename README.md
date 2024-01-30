c396c77bffb06f204fa57f2632853f15396e7cb8
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-01-30 11:28:34 +0530

    hello

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # c-exam-last
 ![Alt text](<Screenshot 2024-01-30 110139.png>)
+![Alt text](<Screenshot 2024-01-30 112741.png>)
--- index.c
+++ index.c
@@ -1,23 +1,46 @@
-#include <stdio.h>
-void fun(int s, int b){
-    int temp;
-    temp=s;
-    s=b;
-    b=temp;
-    printf("after swaipe=>\n");
-    printf("%d\n",s);
-    printf("%d\n",b);
-}
+//que   1    ----------
 
-int main()
-{
-   printf("befor Swipe => \n");
-   int s=5;
-   int b=3;
+// #include <stdio.h>
+// void fun(int s, int b){
+//     int temp;
+//     temp=s;
+//     s=b;
+//     b=temp;
+//     printf("after swaipe=>\n");
+//     printf("%d\n",s);
+//     printf("%d\n",b);
+// }
 
-   printf("%d\n",s);
-   printf("%d\n",b);
-   fun(s,b);
+// int main()
+// {
+//    printf("befor Swipe => \n");
+//    int s=5;
+//    int b=3;
+
+//    printf("%d\n",s);
+//    printf("%d\n",b);
+//    fun(s,b);
   
-  return 0;
-}
+//   return 0;
+// }
+
+
+
+
+//que   2 ------
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int s=20;
+//     int *p=&s;
+//     int **p2;
+//     p2=&p;
+//     printf("%u %d\n",p2,**p2);
+
+//    return 0;
+// }
